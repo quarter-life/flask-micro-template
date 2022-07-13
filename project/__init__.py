69255c51b6828e20e7605b2f4e5a8460059a7a78
quarterlife <lcishu@163.com>
2022-07-13 17:44:22 +0800

    feat(config):new: 1. 添加配置文件

--- project/__init__.py
+++ project/__init__.py
@@ -3,11 +3,15 @@
 # author:quarterlife
 # datetime:2022/5/18 10:33
 # software: PyCharm
+import os
+
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
+
+    os.makedirs(app.instance_path, exist_ok=True)  # 创建实例文件夹
 
     from . import blueprint
     app.register_blueprint(blueprint.bp)
--- utils/base.py
+++ utils/base.py
@@ -3,5 +3,23 @@
 # author:quarterlife
 # datetime:2022/7/13 16:44
 # software: PyCharm
+# description:  基础工具
 
-#   基础工具
+import os
+
+import yaml
+
+
+def get_config_yaml(file_path):
+    if not os.path.exists(file_path):
+        print('无法找到文件')
+        return None
+
+    with open(file_path, 'r', encoding="utf-8") as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+if __name__ == '__main__':
+    param = '../config/base.yaml'
+    res = get_config_yaml(param)
+    print(res)  # {'test': 1}
